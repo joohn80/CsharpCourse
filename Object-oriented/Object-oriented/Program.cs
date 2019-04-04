208a267c06dd16da9773dad4050687412a57dc68
joohn80 <johnifrog@gmail.com>
2019-04-04 11:25:20 +0200

    The rest of the exercises

---
+++ Object-oriented/Object-oriented/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person John = new Person("John", "Blom", 21);
+            John.speak();
+
+            Console.WriteLine(John);
+
+            Console.WriteLine($"instance { Person.GetInstanceCount() }");
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+    class Person
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+        private static int InstanceCount = 0;
+
+        private static int IncreaseInstanceCount()
+        {
+            return InstanceCount++;
+        }
+        public static int GetInstanceCount()
+        {
+            return InstanceCount;
+        }
+
+        public override string ToString()
+        {
+            return $"{firstname}, {lastname}, {age}";
+        }
+
+        public Person(string firstname, string lastname, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+            IncreaseInstanceCount();
+        }
+
+        public void speak()
+        {
+            Console.WriteLine($"Hello my name is {firstname} {lastname}");
+        }
+
+        class Employee: Person
+        {
+            public double Salary;
+
+            public Employee(double salary)
+            {
+                Salary = salary;
+            }
+        }
+
+        public
+
+        class Client: Person
+        {
+
+        }
+    }
+}
